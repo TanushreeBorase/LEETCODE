b05668fb9c59130fac58490e4e981720103d8411
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2024-03-10 22:48:56 +0530

    Update Intersection of two arrays.cpp

--- Intersection of two arrays.cpp
+++ Intersection of two arrays.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int>v;
-        sort(num1.begin(),nums1.end());
-        sort(num2.begin(),num2.end());
-        int last = -1;
-        while(i<num1.size() && j<num2.size){
-            if(nums1[1]==num2[i]){
-                v.push_back(nums1[i]);
-                last = num1[i];
+
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        int i=0,j=0;
+        int n=nums1.size();
+        int m =nums2.size();
+        int las =-1;
+        while(i<n && j<m){
+        if(nums1[i]==nums2[j] ){
+            if( las!=nums1[i]){   
+            v.push_back(nums1[i]);
+            las=nums1[i];
             }
             i++;
+            j++; 
+        }else if(nums1[i]>nums2[j]){
             j++;
-        }elseif(nums1[i]>nums2[j]){
-            j++;
-        }else{
+        }else {
             i++;
         }
-    }
-    return v;
+        }
+
+       return v;
     }
 };
