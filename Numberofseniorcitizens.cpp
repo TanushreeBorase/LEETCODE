fce9acf56ff060b01993fa83bc3755b411767205
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2024-08-04 23:24:07 +0530

    Update Numberofseniorcitizens.cpp

--- Numberofseniorcitizens.cpp
+++ Numberofseniorcitizens.cpp
@@ -3,9 +3,17 @@ public:
     int countSeniors(vector<string>& details) {
         int seniorcount = 0;
 
-        for(const string &info:details){
+        for(const string &info:details) 
+            // const string &info declares a constant reference to each string element in the details container, 
+            // meaning info is a read-only reference to each element.   
+        {
             int age = stoi(info.substr(11,2));  
-
+            /*
+                info.substr(11, 2) calls the substr method on the string object info.
+                substr(11, 2) extracts a substring starting from index 11 of info and takes 2 characters.
+                stoi is a standard library function that converts a string to an int.
+                stoi(info.substr(11, 2)) converts the extracted substring (which should represent a number) to an integer and assigns it to the variable age.
+            */
             if(age > 60){
                 seniorcount++;
             }
